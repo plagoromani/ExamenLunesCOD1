031797f3f30d38ef3f12585de80367c42c3cf621
Pablite5 <Pablite5@GAMING>
2016-06-06 08:56:04 +0200

    Refactor nombre

---
+++ src/examenfinalparte1/Métodos.java
@@ -0,0 +1,21 @@
+package examenfinalparte1;
+/**
+ * 
+ * @author Pablite5
+ */
+public class Métodos {
+    int i;
+
+    
+    public Métodos(int j, int m) {
+        i=met(j,m);
+    }
+    public Métodos(int j) {
+        i=met(j,j);
+    }
+    
+    public Métodos() {
+        i=-1;
+    }
+    public int met(int x, int x1) {
+      return x + x1;}}
