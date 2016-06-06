7acd79d05efe82ee82df41657292c6bc642869e5
Pablite5 <Pablite5@GAMING>
2016-06-06 09:22:02 +0200

    Javadoc hecho

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,13 +1,41 @@
 package examenfinalparte1;
 import java.io.IOException;
+/**
+ *
+ * @author Pablite5
+ */
+
+
 public class Prin {
-public static void main(String[] args) throws IOException{
-Principal obj = new Principal(10,20);
-int n = obj.i;
-System.out.println("Es: " + n);
-Principal nm = new Principal(10);
-int m = nm.i;
-System.out.println("Es: " + m);
-Principal mn = new Principal();
-int nn = mn.i;
-System.out.println("Es: " + nn);}}
+/**
+ * En esta clase, que es la main se llaman a los métodos de la clase Métodos.
+ * @param args
+ * @throws IOException 
+ */
+    
+    public static void main(String[] args){
+
+        /**
+         * Creamos el objeto métodos y le damos los valores de j y de m.
+         */
+        Métodos obj = new Métodos(10,20);
+
+     /**
+      * Esta es la primera operación, como es el metodo i la suma va a ser de los dos parámetros, en este caso j=10 y m=20 y se muestra por pantalla el resultado
+      */
+        int operacion1 = obj.i;
+
+        System.out.println("Es: " + operacion1);
+     /**
+      * Esta es la segunda operación, como es el método z va a utilizar solo el parametro j, en este caso j=10, que es el valor que le damos al principio, y seguidamente lo mostramos por pantalla
+      */
+         int operacion2 = obj.z;
+
+        System.out.println("Es: " + operacion2);
+     /**
+      * Esta es la tercera operacion, como es el método w va a valer siempre -1 ya que le hemos dado el valor en el propio método y siempre va a tener ese valor. Luego lo mostramos por pantalla.
+      */
+     
+        int operacion3 = obj.w;
+
+        System.out.println("Es: " + operacion3);}}
