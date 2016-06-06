3dcffb7449dfd01b7e1e4c7fd5bc6ca16e440aaa
Pablite5 <Pablite5@GAMING>
2016-06-06 09:25:26 +0200

    Finalizado

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -23,18 +23,15 @@ public class Prin {
       * Esta es la primera operación, como es el metodo i la suma va a ser de los dos parámetros, en este caso j=10 y m=20 y se muestra por pantalla el resultado
       */
         int operacion1 = obj.i;
-
         System.out.println("Es: " + operacion1);
      /**
       * Esta es la segunda operación, como es el método z va a utilizar solo el parametro j, en este caso j=10, que es el valor que le damos al principio, y seguidamente lo mostramos por pantalla
       */
          int operacion2 = obj.z;
-
-        System.out.println("Es: " + operacion2);
+         System.out.println("Es: " + operacion2);
      /**
       * Esta es la tercera operacion, como es el método w va a valer siempre -1 ya que le hemos dado el valor en el propio método y siempre va a tener ese valor. Luego lo mostramos por pantalla.
       */
      
         int operacion3 = obj.w;
-
         System.out.println("Es: " + operacion3);}}
