5d5db20bbac067c7d573323befc79e604d567992
Pablite5 <Pablite5@GAMING>
2016-06-18 12:07:51 +0200

    Vuelta a un paso anterior en el método para no cambiar el programa

--- src/examenfinalparte1/Métodos.java
+++ src/examenfinalparte1/Métodos.java
@@ -5,21 +5,30 @@ package examenfinalparte1;
  */
 public class Métodos {
     int i;
-    int z;
-    int w;
+   
 
    /**
     * Meto todas las operaciones en un mismo método, este método realiza las operaciones cuando le damos valores a j y a m desde la main.
     * @param j es uno de los valores que le damos.
     * @param m es otro de los valores que le damos
     */
-    public Métodos(int j, int m) {
+        public int sumaDosVariables (int j, int m) {
         i=suma(j,m); //i realiza una suma con los valores de j y m.
-       
-        z=suma(j,j); //z realiza una suma con el valor de j.
-        
-         w=-1; //w siempre vale -1.
+        return i;
     }
+        public int sumaMismaVariable (int j){
+            i=suma(j,j); // realiza una suma con el valor de j.
+            return i;
+        }
+        public int sumaSiempreIgual (){
+            i=-1;
+            return i;
+        }
+       
+       
+        
+        
+    
      /**
      * Este método realiza una suma.
      * @param x es el primer valor.
@@ -27,6 +36,6 @@ public class Métodos {
      * @return nos devuelve la suma.
      */
     public int suma(int x, int x1) {
-      return x + x1;
+        return x + x1;
     }
 }
